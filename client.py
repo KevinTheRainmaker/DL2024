a9ead066da7a5932edb841423884072296dbb99a
Seung-min Kim <a7965344@gmail.com>
2024-05-09 21:03:11 +0900

    client: update code using session state

--- client.py
+++ client.py
@@ -2,12 +2,22 @@ import streamlit as st
 import time
 import numpy as np
 from PIL import Image
+from streamlit import session_state as ss
 
 st.set_page_config(layout="wide")
 empty1,con1,empty2 = st.columns([0.3,1.0,0.3])
 empyt1,con2,con3,empty2 = st.columns([0.3,0.5,0.5,0.3])
 empyt1,con4,empty2 = st.columns([0.3,1.0,0.3])
 
+if 'upload_file' not in ss:
+    ss['upload_file'] = True
+
+if 'process_img' not in ss:
+    ss['process_img'] = False    
+
+if 'show_result' not in ss:
+    ss['show_result'] = False
+    
 categories = ['강아지', '고양이', '토끼', '공룡', '곰', '사슴', '여우']
 
 def main():
@@ -16,46 +26,61 @@ def main():
     with con1:
         st.title("닮은 동물상 찾기")
         st.subheader("이미지를 업로드하세요.")
-        uploaded_file = st.file_uploader(label="", type=["jpg", "jpeg", "png"], key="file_uploader")
 
-        if uploaded_file is not None:
-            # PIL Image로 변환
-            upload_img = Image.open(uploaded_file)
-            #업로드한 이미지
-            #face_img = process_image(upload_img)
-            face_img = Image.open(uploaded_file)
-            #강아지,고양이,토끼,공룡,곰,사슴,여우
-            with st.spinner('사진을 분류중입니다.'):
-                result = time.sleep(2)
-                #prediction = get_prediction(face_img)
-                predictions = np.random.rand(7)
-                #grad_cam = get_gradcam(face_img)
-                grad_cam = Image.open(uploaded_file)
-                #closet_img, closet_dist = get_closet(face_img)
-                closet_img = Image.open(uploaded_file)
-                closet_dist = np.random.rand(1)
+        if ss['upload_file']:
+            uploaded_file = st.file_uploader(label="", type=["jpg", "jpeg", "png"], key="file_uploader")
+            if uploaded_file is not None:
+                ss['image'] = uploaded_file  # backup the file
+                ss['upload_file'] = False
+                ss['process_img'] = True
+                st.rerun()
                 
+        if ss['process_img']:        
+            # PIL Image로 변환
+            upload_img = Image.open(ss['image'])
+
+            #face_img = process_image(upload_img)
+            face_img = upload_img
+
+            with st.spinner('사진을 분류중입니다.'):
+                #로딩 화면 테스트용 더미 시간
+                time.sleep(2)
+                
+                #prediction = get_prediction(face_img)
+                ss['predictions'] = np.random.rand(7)
+                
+                #grad_cam = get_gradcam(face_img)
+                ss['grad_cam'] = upload_img
+                
+                #closet_img, closet_dist = get_closet(face_img)
+                ss['closet_img'] = upload_img
+                ss['closet_dist'] = np.random.rand(1)
+            ss['process_img'] = False
+            ss['show_result'] = True
+            
+        if ss['show_result']: 
             with con1:
-                col1, col2, col3 = st.columns([1, 3, 1])
-                with col2:
+                _, col, _ = st.columns([1, 3, 1])
+                with col:
                     st.image(face_img, caption='크롭된 얼굴 사진', use_column_width=True)
+                    
             #이미지 결과 출력
             with con2:
-                st.image(grad_cam, caption='Grad-CAM Visualization', use_column_width=True)
+                st.image(ss['grad_cam'], caption='Grad-CAM Visualization', use_column_width=True)
             with con3:
-                st.image(closet_img, caption='가장 비슷한 동물', use_column_width=True)
+                st.image(ss['closet_img'], caption='가장 비슷한 동물', use_column_width=True)
             with con4:
                 # Display the prediction results as progress bars
-                st.write("가장 비슷한 동물상은 **{}** 입니다!".format(categories[np.argmax(predictions)]))
+                st.write("가장 비슷한 동물상은 **{}** 입니다!".format(categories[np.argmax(ss['predictions'])]))
 				#닮은 동물과의 거리
                 col1, col2 = st.columns([1, 3])
                 with col1:
                     st.write("닮은 동물과의 유사도")
                 with col2:
-                    st.progress(closet_dist[0])
+                    st.progress(ss['closet_dist'][0])
                     
                 # Inject custom CSS for each progress bar
-                for category, prob in zip(categories, predictions):
+                for category, prob in zip(categories, ss['predictions']):
                     col1, col2 = st.columns([1, 3])
                     with col1:
                         st.write(category)
