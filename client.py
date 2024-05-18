bcdef55144fbf2945b5faf592ba8a87df889b97a
Kangbeen Ko <sweetrainforyou@gmail.com>
2024-05-18 23:17:17 +0900

    test: print current path

--- client.py
+++ client.py
@@ -51,9 +51,11 @@ os.makedirs('./faiss', exist_ok=True)
 def download_drive_file(output, url):
     gdown.download(url, output, quiet=False)
 
-download_drive_file('model.pth', 'https://drive.google.com/file/d/1sFlYDtHlnd1NF64HODLbk9OX-f4VPMnc/view?usp=sharing')
 download_drive_file('faiss', 'https://drive.google.com/file/d/1QSzpvyfEqYM2dbzLPiwVnZdsExP0rMn3/view?usp=sharing')
 download_drive_file('loading.json', 'https://drive.google.com/file/d/1eXQ2L9J0_sYQlrMbCLm5kM5UDVD6I9Ro/view?usp=sharing')
+
+print(os.getcwd())
+print(os.listdir('./'))
 
 file_name = "faiss.zip"
 output_dir = "faiss"
@@ -180,9 +182,14 @@ def load_model(classes=12):
     model = models.resnet50(pretrained=True)
     num_ftrs = model.fc.in_features
     model.fc = nn.Linear(num_ftrs, classes)
-
+    
+    model_path = '/mount/src/dl2024/model.pth'
+    
+    if not os.path.exists(model_path):
+        download_drive_file('model.pth', 'https://drive.google.com/file/d/1sFlYDtHlnd1NF64HODLbk9OX-f4VPMnc/view?usp=sharing')
+        
     # 학습된 모델의 가중치 로드
-    model.load_state_dict(torch.load('model.pth', map_location=device))
+    model.load_state_dict(torch.load(model_path, map_location=device))
     model.eval()
     return model
 
